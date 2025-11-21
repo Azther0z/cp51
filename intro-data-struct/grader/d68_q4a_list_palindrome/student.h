5739ec9b5b2e6dba6fe285ab1b377b61a13904c4
Azther0z <saintvsboy@gmail.com>
2025-11-21 12:48:10 +0700

    Data structure: Quiz 4

--- intro-data-struct/grader/d68_q4a_list_palindrome/student.h
+++ intro-data-struct/grader/d68_q4a_list_palindrome/student.h
@@ -1,3 +1,5 @@
+// List Longest Palindrome
+
 #ifndef __STUDENT_H__
 #define __STUDENT_H__
 
@@ -5,7 +7,25 @@
 
 template<typename T>
 size_t CP::list<T>::longest_palindrome() {
-  return 0;
+  int mid = (mSize - 1) / 2;
+  node *left = mHeader->next;
+  while (mid--) {
+    left = left->next;
+  }
+  node *right = left->next;
+  if (mSize % 2 != 0) {
+    left = left->prev;
+  }
+  if (left->data != right->data) {
+    return 1;
+  }
+  int result = (mSize % 2 != 0) ? 1 : 0;
+  while (left != mHeader && right != mHeader && left->data == right->data) {
+    result += 2;
+    left = left->prev;
+    right = right->next;
+  }
+  return result;
 }
 
 #endif
