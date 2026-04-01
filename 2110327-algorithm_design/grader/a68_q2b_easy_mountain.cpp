2cf2979186e41bec531b1c6e963c8f7cc65d7259
Azther0z <saintvsboy@gmail.com>
2026-04-01 19:27:16 +0700

    algorithm_design: Quiz 3

--- 2110327-algorithm_design/grader/a68_q2b_easy_mountain.cpp
+++ 2110327-algorithm_design/grader/a68_q2b_easy_mountain.cpp
@@ -4,24 +4,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// As 1<=a[i]<=100, optimal solution is to choose latest value of a[i].
-// From Pigeonhole Principle, if we look back more than 100 step,
-// there will be only unoptimal solution.
-// Therefore, we only look back no more than 100 steps
-// R(n) = max{R(n-i)+(a[n]+a[n-i])^3|1<=i<=100}
+// R(a) = max{R(i)+(i+a)^3|1<=i<=100}
 
 int main() {
     int n;
     cin >> n;
-    vector<long long> a(n + 1);
+    vector<int> a(n + 1);
     for (int i = 1; i <= n; i++) {
         cin >> a[i];
     }
-    vector<long long> memo(n + 1, 0);
+    vector<long long> memo(101, -1);
+    long long result = 0;
+    memo[a[1]] = 0;
     for (int i = 2; i <= n; i++) {
-        for (int j = 1; j <= 100 && i - j >= 1; j++) {
-            memo[i] = max(memo[i], memo[i - j] + (a[i] + a[i - j]) * (a[i] + a[i - j]) * (a[i] + a[i - j]));
+        long long tmp = 0;
+        for (int j = 1; j <= 100; j++) {
+            if (memo[j] != -1) {
+                tmp = max(tmp, memo[j] + (a[i] + j) * (a[i] + j) * (a[i] + j));
+            }
         }
+        memo[a[i]] = max(memo[a[i]], tmp);
+        result = max(result, memo[a[i]]);
     }
-    cout << memo[n];
+    cout << result;
 }
